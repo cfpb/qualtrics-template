df0fc68eebfda9fccda63cddd381b971a744a95f
Wyatt Pearsall <wyatt.pearsall@cfpb.gov>
2022-07-18 08:58:19 -0400

    Remove upload workflow, add upload script

---
+++ s3.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+function putS3 {
+  dir=$1
+  file=$2
+  content_type="text/$3"
+  aws_prefix='/assets/qualtrics/'
+  bucket='files.consumerfinance.gov'
+  curr_date=$(date +"%a, %d %b %Y %T %z")
+  string="PUT\n\n$content_type\n$curr_date\n/$bucket$aws_prefix$file"
+  signature=$(echo -en "${string}" | openssl sha1 -hmac "$AWS_SECRET" -binary | base64)
+  curl -X PUT -k -T "$dir/$file" \
+    -H "Host: $bucket.s3.amazonaws.com" \
+    -H "Date: $curr_date" \
+    -H "Content-Type: $content_type" \
+    -H "Authorization: AWS $AWS_ID:$signature" \
+    "https://$bucket.s3.amazonaws.com$aws_prefix$file"
+}
+
+echo "${AWS_ID:?AWS access key id unset}" > /dev/null
+echo "${AWS_SECRET:?AWS secret access key unset}" > /dev/null
+
+for file in src/*; do
+  putS3 "src" "${file##*/}" "${file##*.}"
+  echo "Uploaded $file"
+done
